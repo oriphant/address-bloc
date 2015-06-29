68d8f93cfbd87f1585ac303cd6743361633ebf8f
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-06-28 18:23:25 -0700

    CP22-Assignment Complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -11,10 +11,11 @@ attr_accessor :address_book
         puts "Main Menu - #{@address_book.entries.count} Entries"
         puts "\n"
         puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - View Entry Number n"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         puts "\n"
         print "Enter your selection: "
 
@@ -27,17 +28,21 @@ attr_accessor :address_book
             main_menu
         when 2
             system "clear"
-            create_entry
+            view_entry_n
             main_menu
         when 3
             system "clear"
-            search_entries
+            create_entry
             main_menu
         when 4
             system "clear"
-            read_csv
+            search_entries
             main_menu
         when 5
+            system "clear"
+            read_csv
+            main_menu
+        when 6
             puts "Good-bye!"
             exit(0)
         else
@@ -57,6 +62,35 @@ attr_accessor :address_book
         system "clear"
         puts "End of entries"
     end
+
+# ~~~~~ CP22 Assignment Here ~~~~~~~~~
+    def view_entry_n
+        system "clear"
+    
+        if @address_book.entries.length ==0
+            puts "You need to first enter an address using Option 3 below"
+            puts"\n"
+            main_menu
+        else
+            print"Which entry would you like to see?: "
+            entry_n = gets.to_i
+        
+                while entry_n>@address_book.entries.length || entry_n<=0
+                    puts"\n"
+                    puts"Invalid entry there are only #{@address_book.entries.length} entries."
+                    puts"Please enter a new number less than or equal to #{@address_book.entries.length}: "
+                    entry_n = gets.chomp.to_i
+                end
+                    system "clear"
+                    output_n = @address_book.entries[entry_n-1]
+                    puts"\n"
+                    puts "This is the contact:"
+                    puts "#{output_n}"
+                    puts"\n"
+        end
+        
+    end
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def create_entry
         system "clear"
